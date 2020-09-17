5274ba024546d01582d8fe25be3c0b3da51e2c68
FejsalA <71178550+FejsalA@users.noreply.github.com>
2020-09-17 09:10:05 +0200

    Add files via upload

---
+++ IntroductionProgramming1/Assigment1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Assigment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name;
+            string age;
+
+            Console.Write("Enter your name: ");
+
+            name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+
+            age = Console.ReadLine();
+
+
+
+         
+           Console.WriteLine("Your name is " + name);
+           Console.WriteLine("Your age is " + age);
+
+           Console.ReadKey();
+        }
+    }
+}
---
+++ IntroductionProgramming1/Assigment2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assigment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            string input = Console.ReadLine();
+
+
+            int age = int.Parse(input);
+
+
+            age += 1;
+
+          string output = age.ToString();
+
+           Console.WriteLine("Next year you will be " + age  + " years old");
+
+
+            Console.ReadKey(); 
+        }
+    }
+}
